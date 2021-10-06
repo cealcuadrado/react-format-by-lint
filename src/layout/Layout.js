b828b67389b7271ed708b4819bf32243aeacba93
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-06 19:50:32 -0300

    Instaladas dependencias de formateo, definido layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,7 +3,56 @@ import React from "react";
 class Layout extends React.Component {
     render() {
         return (
-            <p>Hola, mundo!</p>
+          <div>
+                <h1>Hola, mundo!</h1>
+                
+                <p>
+                    
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              faucibus pellentesque felis non ornare. Maecenas pharetra nibh id
+              odio fermentum, a consectetur magna aliquam. Proin feugiat eros
+              dapibus eros rhoncus imperdiet. Sed vehicula cursus commodo. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus
+              justo, egestas id diam at, consequat ultricies ligula. Praesent
+              rutrum ipsum vestibulum, mattis dui eu, ullamcorper augue. Vivamus
+              pulvinar blandit bibendum. In tincidunt efficitur erat vel
+              imperdiet. Mauris pharetra ultricies viverra. Quisque vestibulum
+                    fermentum nibh ut elementum.
+                    
+            </p>
+
+                <p>
+                    
+              Maecenas pellentesque, odio ac consectetur tempor, ante lectus
+              tempus ipsum, sagittis volutpat quam libero eget lectus. In in
+              ipsum sollicitudin felis sagittis bibendum ut aliquam turpis. Cras
+              vel eros metus. Ut ultricies sapien metus, vitae tristique neque
+              ultrices non. Duis egestas quis ligula ut vulputate. Quisque
+              efficitur urna vel felis volutpat fermentum. Maecenas et malesuada
+              erat, cursus congue nisl. Pellentesque varius, diam ut commodo
+              fermentum, arcu dolor volutpat metus, ac dignissim dolor augue
+              eget mi. Praesent luctus lectus vel libero sodales sollicitudin.
+              Morbi sodales commodo dolor nec auctor. Donec vel dui venenatis,
+              sagittis sem fringilla, suscipit ligula. Proin id risus vitae
+              turpis feugiat cursus. Donec id dapibus eros, eu rhoncus leo. Nam
+                    posuere dictum est id tempor.
+                    
+            </p>
+
+                <p>
+                    
+              urabitur porttitor lacus eget justo interdum blandit. Nulla
+              rhoncus auctor scelerisque. Nullam vehicula lacus eu lorem
+              sollicitudin, at tempor arcu cursus. Cras ornare neque at mattis
+              consequat. Sed aliquet facilisis tortor eget pretium. Aenean
+              mattis pharetra faucibus. Vivamus nec est eu arcu egestas mattis
+              at eget augue. Etiam iaculis velit non tempus hendrerit. Phasellus
+              consectetur suscipit pretium. Aliquam quis tristique leo, et
+              hendrerit elit. Quisque vitae dignissim augue. Phasellus vel enim
+                    orci.
+                    
+            </p>
+          </div>
         );
     }
 }
